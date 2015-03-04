f63cf90461e9db3cb377b9769563db9fa4826360
Anastasiya Sakharova <elliot.wrt@gmail.com>
2015-03-04 17:26:46 +0500

    remove some blocks

--- static/pages/views/index.jsx
+++ static/pages/views/index.jsx
@@ -3,7 +3,6 @@ var DefaultLayout = require('../layouts/default/main'),
     ForecastToday= require('./forecast.today.jsx'),
     ForecastFull = require('./forecast.full.jsx'),
     ForecastShort = require('./forecast.short.jsx'),
-    CityDropDown = require('./city.dropdown.jsx'),
     ForecastHourly = require('./forecast.hourly.jsx'),
     DetailsMenu = require('./details.menu.jsx');
 
@@ -15,7 +14,6 @@ var Index = React.createClass({
                 <ForecastToday today={this.props.today}></ForecastToday>
                 <ForecastFull full={this.props.full}></ForecastFull>
                 <ForecastShort short={this.props.short}></ForecastShort>
-                <CityDropDown></CityDropDown>
                 
                 <div id="hint__forecast-base" className="hint" style={{"top": "198px", "left": "524px", "display": "none"}}>
                     <p className="hint__text">На основе прогноза</p>
